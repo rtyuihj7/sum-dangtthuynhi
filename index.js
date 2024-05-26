109a4c819b56e29e1edcf363a3fd9504c8620be6
rtyuihj7 <163233209+rtyuihj7@users.noreply.github.com>
2024-05-26 21:27:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ function sumIntegers(...args) {
     return args.reduce((acc, val) => acc + (Number.isInteger(val) ? val : 0), 0);
 }
 
+function sumFloats(...args) {
+    return args.reduce((acc, val) => acc + (typeof val === 'number' ? val : 0), 0);
+}
+
+
 exports.sumIntegers = sumIntegers;
